527f2df341b969e70a83f730f01c4e1b8c276017
Gaël Reyrol <me@gaelreyrol.dev>
2021-09-23 13:53:51 +0200

    feat: add socket.io & pulsar

--- server/index.js
+++ server/index.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const http = require('http');
 const morgan = require('morgan');
+const { Server } = require('socket.io');
 
 require('dotenv').config();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = process.env.PORT || 3000;
 
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-    res.send('hello, world!')
+    res.send('Hello Devoxx Paris 2021!');
 });
 
-app.listen(port, () => {
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(port, function () {
     console.log(`App listening at http://localhost:${port}`)
 });
