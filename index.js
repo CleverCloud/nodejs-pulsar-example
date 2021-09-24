d4322564b9b90bec8f437040bff76d6f81499e1f
Gaël Reyrol <me@gaelreyrol.dev>
2021-09-24 15:21:36 +0200

    feat: add message analysis

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const Pulsar = require('pulsar-client');
 const morgan = require('morgan');
 const { Server } = require('socket.io');
+const fetch = require('node-fetch');
 
 require('dotenv').config();
 
@@ -19,24 +20,27 @@ const pulsarClient = new Pulsar.Client({
 const pulsar = {
     namespace: process.env.ADDON_PULSAR_NAMESPACE,
     tenant: process.env.ADDON_PULSAR_TENANT,
-    topic: process.env.PULSAR_TOPIC,
+    topics: {
+        raw: process.env.PULSAR_TOPIC_RAW,
+        analyzed: process.env.PULSAR_TOPIC_ANALYZED,
+    }
 }
 
 /**
  * @returns {Promise<Pulsar.Producer>}
  */
-function createProducer() {
+function createProducer(topic) {
     return pulsarClient.createProducer({
-        topic: `persistent://${pulsar.namespace}/${pulsar.tenant}/${pulsar.topic}`,
+        topic: `persistent://${pulsar.namespace}/${pulsar.tenant}/${topic}`,
     });
 }
 
 /**
  * @returns {Promise<Pulsar.Consumer>}
  */
-function createConsumer(listener) {
+function createConsumer(topic, listener) {
     return pulsarClient.subscribe({
-        topic: `persistent://${pulsar.namespace}/${pulsar.tenant}/${pulsar.topic}`,
+        topic: `persistent://${pulsar.namespace}/${pulsar.tenant}/${topic}`,
         subscription: 'socket-io',
         subscriptionType: 'Shared',
         ackTimeoutMs: 10000,
@@ -44,16 +48,50 @@ function createConsumer(listener) {
     });
 }
 
+function getSentimentAnalysis(text, model = 'distilbert-base-uncased-finetuned-sst-2-english') {
+    return fetch(process.env.ANALYSIS_API + `?text=${encodeURIComponent(text)}&model=${model}`, {
+        method: 'POST',
+    });
+}
+
+async function analyseMessages(rawTopic, analyzedTopic) {
+    const analyzedProducer = await createProducer(analyzedTopic);
+
+    await createConsumer(rawTopic, async (msg, msgConsumer) => {
+        const message = msg.getData().toString();
+
+        console.log('Consume raw message:', { message });
+
+        const response = await getSentimentAnalysis(message).catch((err) => {
+            console.error({ err });
+            return ;
+        });
+
+        const data = await response.json();
+
+        console.log('Analyzed raw message:', { data });
+
+        await analyzedProducer.send({
+            data: Buffer.from(JSON.stringify({ message, data })),
+        }).catch((err) => {
+            console.error({ err });
+            return ;
+        });
+
+        console.log('Produced analyzed message.');
+
+        msgConsumer.acknowledge(msg);
+    });
+}
+
 /**
  * @type {Pulsar.Producer}
  */
-let pulsarProducer = null;
+let rawPulsarProducer = null;
 
-createProducer().then((producer) => {
-    pulsarProducer = producer;
+createProducer(pulsar.topics.raw).then((producer) => {
+    rawPulsarProducer = producer;
 });
-
-console.log({ pulsarProducer });
 
 app.use(morgan('combined'));
 app.use(express.static('dist'));
@@ -64,12 +102,14 @@ app.get('/', (req, res) => {
 });
 
 app.post('/messages', async (req, res) => {
-    const msg = await pulsarProducer.send({
+    const msg = await rawPulsarProducer.send({
         data: Buffer.from(req.body.message),
     }).catch((err) => {
         console.error(err);
         res.json({ message: err }).status(500);
     });
+
+    console.log('Produced raw message:', { message: req.body.message });
 
     res.json({ message: msg.toString() });
 });
@@ -77,10 +117,12 @@ app.post('/messages', async (req, res) => {
 io.on('connection', async (socket) => {
     console.log('user connected');
 
-    const consumer = await createConsumer((msg, msgConsumer) => {
+    const consumer = await createConsumer(pulsar.topics.analyzed, (msg, msgConsumer) => {
         const data = msg.getData().toString();
         io.emit('message', data);
         msgConsumer.acknowledge(msg);
+
+        console.log('Consumed analyzed message:', { data });
     });
 
     socket.on('disconnect', () => {
@@ -98,5 +140,7 @@ server.on('close', async () => {
 });
 
 server.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`);
+
+    analyseMessages(pulsar.topics.raw, pulsar.topics.analyzed);
 });
